c2cb25205f8299f44db76fae25fbd2f1d9fc4cc9
Dennis Jackson <dennisdjack1987@gmail.com>
2022-07-26 16:17:26 -0700

    add pass gen

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function getLen(){
-  let len = prompt('lenght of passoword\n 8-128')
+  let len = prompt('lenght of passoword\n 8-128').trim()
   if(parseInt(len) < 8){
     alert('must be at least 8');
     getLen()
@@ -29,7 +29,7 @@ function getLen(){
  }
 
  function getSpecialChar(){
-  specialChar = ['!','@','#','$','&','*']
+  const specialChar = '!@#$%^&*?-_'
   let choice = confirm('Do you want to use speical characters')
   if(choice){
     return specialChar
@@ -38,10 +38,67 @@ function getLen(){
   }
  }
 
+ function getLower(){
+   let lst = 'abcdefghijklmnopqrstvwxyz'
+   let value = confirm('LOWER Case characters?')
+   if(value){
+    return lst
+  }else{
+    return value;
+  }
+ }
+
+ function getUpper(){
+  let lst = 'ABCUDEFGHIJKLMNOPQRSTUVWXYZ'
+  let value = confirm('upper Case characters?')
+  if(value){
+   return lst
+ }else{
+   return value;
+ }
+}
+
+function GetNum(){
+  let lst = '123456789'
+  let value = confirm('Numbers?')
+  if(value){
+   return lst
+ }else{
+   return value;
+ }
+}
+
+
 
 
 function generatePassword(){
- 
+  let pass = ''
+  let lst = []
+  let len = getLen()
 
-  return getLen()
+  let specialChar = getSpecialChar()
+
+  let lower = getLower()
+
+  let upper = getUpper()
+
+  if(upper){
+    lst.push(...upper)
+  }
+  if(lower){
+    lst.push(...lower)
+  }
+  if(specialChar){
+    lst.push(...specialChar)
+  }
+
+  
+  for(let i = 0; i < len; i++){
+    pass += lst[Math.floor(Math.random()*lst.length)]
+  }
+ 
+  return  pass
+
+
+  
 }
